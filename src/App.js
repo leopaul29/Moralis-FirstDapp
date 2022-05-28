35e873b0c3462ce6229eab82a603f23934a16c41
Leo-paul <leopaulmartin@hotmail.fr>
2022-05-28 16:12:48 +0200

    :sparkles: add header + coin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-
+import { ConnectButton } from "web3uikit";
+import logo from "./images/Moralis.png";
+import Coin from "./components/Coin";
 
 const App = () => {
 
+const [btc, setBtc] = useState(80);
+
   return (
     <>
-     
+     <div className="header">
+        <div className="logo">
+            <img src={logo} alt="logo" height="50px"/> Sentiment
+        </div>
+        <ConnectButton/>
+     </div>
+     <div className="instructions">
+        Where do you think these tokens are going? Up or Down?
+     </div>
+
+    <div className="list">
+         <Coin
+          perc={btc}
+          setPerc={setBtc}
+          token={"BTC"}
+         />
+     </div>
     </>
   );
 };
--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,14 +1,59 @@
 import React, { useEffect, useState } from "react";
 import "./Coin.css";
+import { Button } from "web3uikit";
 
+function Coin({perc, setPerc, token}) {
 
-function Coin() {
+    const [color, setColor] = useState();
 
-
+    useEffect(() => {
+        if(perc < 50) {
+            setColor("#c43d08");
+        }else {
+            setColor("green");
+        }
+    }, [perc])
 
   return (
     <>
-      
+        <div>
+          <div className="token">
+            {token}
+          </div>
+
+          <div className="circle"
+            style={{boxShadow: `0 0 20px ${color}`}}>
+              <div className="wave"
+                  style={{
+                      marginTop: `${100 - perc}%`,
+                      boxShadow: `0 0 20px ${color}`,
+                      backgroundColor: color,
+                  }}>
+              </div>
+              <div className="percentage">
+                {perc}
+              </div>
+          </div>
+
+
+          <div className="votes">
+
+            <Button
+                onClick={() => {setPerc(perc + 1)}}
+                text="Up"
+                theme="primary"
+                type="button"
+            />
+            <Button
+                color="red"
+                onClick={() => {setPerc(perc - 1)}}
+                text="Down"
+                theme="colored"
+                type="button"
+            />
+
+          </div>
+        </div>
     </>
   );
 }
